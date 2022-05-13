dedebc6de66a3b0c03cd5764daded8d2f5e16042
Júlio César <74342193+jlcesarr@users.noreply.github.com>
2022-05-13 16:10:16 -0300

    feat(react): added webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle[hash].js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [path.resolve(__dirname, "node_modules")],
+      },
+    ],
+  },
+};
