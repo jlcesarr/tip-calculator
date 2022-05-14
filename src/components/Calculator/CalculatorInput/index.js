193b35438c1a39cd39dbfe94445456e95bae6778
Júlio César <74342193+jlcesarr@users.noreply.github.com>
2022-05-13 23:16:30 -0300

    feat(react): updated calculator input component
    updated styled components and the class component

--- src/components/Calculator/CalculatorInput/index.js
+++ src/components/Calculator/CalculatorInput/index.js
@@ -1,24 +1,77 @@
 import React from "react";
-import { Container, Label, Input, Percentages, Tip, TipInput } from "./styles";
+import {
+  Container,
+  Label,
+  Input,
+  Percentages,
+  Tip,
+  TipInput,
+  ErrorMessage,
+} from "./styles";
 
 export default class CalculatorInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.defaultTips = [5, 10, 15, 25, 50];
+  }
+
   render() {
+    let {
+      onBillValueChange,
+      onTipValueChange,
+      peoplesValue,
+      onPeoplesValueChange,
+      billValue,
+      tipValue,
+    } = this.props;
+    const isValidPeoplesValue = peoplesValue > 0;
+    const defaultTips = this.defaultTips;
+
     return (
       <Container>
         <Label>Bill</Label>
-        <Input />
+        <Input
+          type="number"
+          border
+          onChange={({ target }) => {
+            onBillValueChange(target.value);
+          }}
+          value={billValue}
+        />
         <Label mt={"48px"}>Select Tip %</Label>
-        <Percentages>
-          <Tip>5%</Tip>
-          <Tip>10%</Tip>
-          <Tip>15%</Tip>
-          <Tip>25%</Tip>
-          <Tip>50%</Tip>
-          <TipInput as="input" placeholder="Custom"></TipInput>
+        <Percentages
+          onClick={({ target }) => {
+            onTipValueChange(target);
+          }}
+        >
+          {defaultTips.map((tipPercentage) => {
+            let isActive = Number(tipValue) == tipPercentage;
+            return (
+              <Tip
+                key={tipPercentage}
+                selected={isActive}
+                data-percentage={tipPercentage}
+              >
+                {tipPercentage}%
+              </Tip>
+            );
+          })}
+          <TipInput
+            as="input"
+            placeholder="Custom"
+            onChange={({ target }) => onTipValueChange(target)}
+          ></TipInput>
         </Percentages>
 
         <Label mt={"48px"}>Number of People</Label>
-        <Input />
+        {!isValidPeoplesValue && <ErrorMessage>Can't be zero</ErrorMessage>}
+        <Input
+          value={peoplesValue}
+          onChange={({ target }) => onPeoplesValueChange(target.value)}
+          valid={isValidPeoplesValue}
+          type="number"
+        />
       </Container>
     );
   }
--- src/components/Calculator/CalculatorInput/styles.js
+++ src/components/Calculator/CalculatorInput/styles.js
@@ -1,13 +1,14 @@
 import styled, { css } from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+`;
 
 export const Label = styled.label`
-  font-size: 16px;
   color: ${({ theme }) => theme.grayish.darkCyan};
   display: block;
-  margin-bottom: ${(props) => (props.mb ? props.mb : "16px")};
-  margin-top: ${(props) => (props.mt ? props.mt : "16px")};
+  margin-bottom: ${(props) => props.mb || "16px"};
+  margin-top: ${(props) => props.mt || "16px"};
 `;
 
 export const Input = styled.input`
@@ -22,6 +23,30 @@ export const Input = styled.input`
   color: ${({ theme }) => theme.darkCyan};
   font-weight: bold;
   font-family: "Space Mono";
+  outline: none;
+  border: 3px solid transparent;
+
+  ${(props) =>
+    props.valid == false
+      ? css`
+          border: 2px solid red;
+        `
+      : null}
+
+  ${(props) =>
+    props.border
+      ? `
+          &:hover, &:focus{
+            border-color: ${props.theme.strongCyan};
+          }
+        `
+      : null}
+
+${({ theme }) => `
+    @media only screen and (max-width: ${theme.breakPoints.medium}) {
+      max-width: 100%;
+    }
+  `}
 `;
 
 export const Percentages = styled.div`
@@ -29,10 +54,17 @@ export const Percentages = styled.div`
   grid-template-columns: repeat(3, minmax(100px, 100px));
   gap: 16px;
   max-width: 379px;
+
+  ${({ theme }) => `
+    @media only screen and (max-width: ${theme.breakPoints.medium}) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  `}
 `;
 
 export const Tip = styled.div`
-  background-color: ${({ theme }) => theme.darkCyan};
+  background-color: ${(props) =>
+    props.selected ? props.theme.strongCyan : props.theme.darkCyan};
   height: 48px;
   font-size: ${({ theme }) => theme.input.fontSize};
   color: ${({ theme }) => theme.white};
@@ -40,16 +72,40 @@ export const Tip = styled.div`
   text-align: center;
   padding: 5px 0;
   cursor: pointer;
+  transition: 0.3s ease-in;
 `;
 
 export const TipInput = styled(Tip)`
   outline: none;
   border: none;
+  width: 100%;
+  display: block;
   background-color: ${({ theme }) => theme.grayish.veryLightCyan};
-
+  transition: unset;
+  text-align: right;
+  padding: 0 5px;
+  border: 3px solid transparent;
+  /* caret-color: ${({ theme }) => theme.grayish.veryLightCyan}; */
   &,
   ::placeholder {
-    color: ${({ theme }) => theme.grayish.darkCyan};
-    font-weight: bold;
+    color: ${({ theme }) => theme.darkCyan};
+    font-weight: bolder;
   }
+
+  ::placeholder {
+    text-align: center !important;
+    font-size: 22px;
+  }
+
+  &:focus,
+  &:hover {
+    background-color: unset;
+    border-color: ${({ theme }) => theme.strongCyan};
+  }
+`;
+
+export const ErrorMessage = styled.span`
+  /* margin-left: 190px; */
+  float: right;
+  color: ${({ theme }) => theme.errorMessage};
 `;
