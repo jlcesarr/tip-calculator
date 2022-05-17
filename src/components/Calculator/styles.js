ca0558b56dff94d24f5d79ff0f7afcce9cb0c95f
Júlio César <74342193+jlcesarr@users.noreply.github.com>
2022-05-17 15:31:38 -0300

    refactor(react): refactored styled component from calculator component

--- src/components/Calculator/styles.js
+++ src/components/Calculator/styles.js
@@ -2,15 +2,16 @@ import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   max-width: 920px;
-  background-color: ${({ theme }) => theme.white};
   width: 100%;
   min-height: 481px;
-  border-radius: ${({ theme }) => theme.borderRadius};
   padding: 42px 32px;
   display: flex;
   column-gap: 56px;
 
-  ${({ theme }) => console.log(theme.breakPoints.medium)}
+  ${({ theme }) => css`
+    background-color: ${theme.white};
+    border-radius: ${theme.borderRadius};
+  `};
 
   ${({ theme }) => `
     @media only screen and (max-width: ${theme.breakPoints.medium}) {
